3c1fd1d7b32d9aea3d80706e36bf2dd67e6137a4
Jelle <jelle.bouwhuis@octo3.fi>
2016-01-30 00:31:09 +0200

    Fixed a silly mistake

--- UnityProject/PandaTree/Assets/Scripts/Inventory/Eatable.cs
+++ UnityProject/PandaTree/Assets/Scripts/Inventory/Eatable.cs
@@ -3,10 +3,10 @@ using System.Collections;
 
 public class Eatable : Item{
 
-    protected bool eatable = true; 
+    protected bool isEatable = true; 
     protected int restoreAmount; // Amount of life energy the food restores
 
-    public bool Eatable { get { return this.eatable; } }
+    public bool IsEatable { get { return this.isEatable; } }
     public int RestoreAmount { get { return this.restoreAmount; } }
 
 	// Use this for initialization
--- UnityProject/PandaTree/Assets/Scripts/Inventory/NonEatable.cs
+++ UnityProject/PandaTree/Assets/Scripts/Inventory/NonEatable.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class NonEatable : Item {
 
+    protected bool isEatable = false; 
+
 	// Use this for initialization
 	void Start () {
 	
