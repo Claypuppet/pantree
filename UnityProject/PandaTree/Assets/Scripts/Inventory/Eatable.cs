c34b7b1edeb078eb1827096c30275c01996dd478
Jelle <jelle.bouwhuis@octo3.fi>
2016-01-30 00:20:46 +0200

    Add Inventory item objects

---
+++ UnityProject/PandaTree/Assets/Scripts/Inventory/Eatable.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Eatable : Item{
+
+    protected bool eatable = true; 
+    protected int restoreAmount; // Amount of life energy the food restores
+
+    public bool Eatable { get { return this.eatable; } }
+    public int RestoreAmount { get { return this.restoreAmount; } }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
