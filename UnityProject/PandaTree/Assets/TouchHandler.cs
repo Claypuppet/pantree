60f19797914387606e87ffaddd8baa635deda814
Sami Repo <sami.repo@octo3.fi>
2016-01-30 16:32:04 +0200

    ThumbSticking

---
+++ UnityProject/PandaTree/Assets/TouchHandler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchHandler : MonoBehaviour {
+
+	private Vector2 touchPos;
+	public GameObject thumbStick;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount < 0)
+			return;
+		touchPos = Input.GetTouch (0).position;
+
+
+	}
+
+	void SummonThumbstick(Vector2 pos){
+		
+	}
+}
