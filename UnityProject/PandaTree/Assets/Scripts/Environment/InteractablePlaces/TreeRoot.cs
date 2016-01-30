0434460a93bdfa62204e2aeb12e78a648ba67c0d
Jelle <jelle.bouwhuis@octo3.fi>
2016-01-30 13:42:57 +0200

    remove csproj

--- UnityProject/PandaTree/Assets/Scripts/Environment/InteractablePlaces/TreeRoot.cs
+++ UnityProject/PandaTree/Assets/Scripts/Environment/InteractablePlaces/TreeRoot.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TreeRoot : MonoBehaviour {
+public class TreeRoot : InteractablePlace {
 
 	// Use this for initialization
 	void Start () {
