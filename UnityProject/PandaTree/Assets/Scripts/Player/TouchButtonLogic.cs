9369bbdaa93059273e04ce09d69c97012d3b598d
Marthijn1994 <marthijn1994@gmail.com>
2016-01-30 13:38:03 +0200

    Added Joystick script.

---
+++ UnityProject/PandaTree/Assets/Scripts/Player/TouchButtonLogic.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchButtonLogic : MonoBehaviour {
+	
+	void Update ()
+    {
+	    if (Input.touches.Length <= 0)
+        {
+            // if no touches then execute this code.
+        }
+        else
+        {
+            // loop through all the touches on screen
+            for (int i = 0; i < Input.touchCount; i++)
+            {
+                //execute this code for current touch on screen.
+
+            }
+        }
+	}
+}
