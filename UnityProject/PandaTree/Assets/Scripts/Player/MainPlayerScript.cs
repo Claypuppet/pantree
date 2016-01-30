02a59f722363d93b30e061e151d334de9378a2d2
Jelle <jelle.bouwhuis@octo3.fi>
2016-01-30 11:30:37 +0200

    add MainPlayerScript

---
+++ UnityProject/PandaTree/Assets/Scripts/Player/MainPlayerScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class MainPlayerScript : MonoBehaviour {
+
+    private List<Item> inventory;
+    private List<Item> offered;
+    private int lifeEnergy;
+
+	// Use this for initialization
+	void Start () {
+        this.inventory = new List<Item>();
+        this.offered = new List<Item>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
