3aafeda202319bdda2d815819ef74fb8c090cadc
Jelle <jelle.bouwhuis@octo3.fi>
2016-01-30 10:06:56 +0200

    Add Evnvironment scripts

---
+++ UnityProject/PandaTree/Assets/Scripts/Environment/Seasons/Season.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Season : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- UnityProject/PandaTree/Assets/Scripts/GameHandler.cs
+++ UnityProject/PandaTree/Assets/Scripts/GameHandler.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameHandler : MonoBehaviour {
 
+    private int playerScore;
+
+    public int PlayerScore { get { return this.playerScore; } }
+
 	// Use this for initialization
 	void Start () {
-	
+        this.playerScore = 0;
 	}
 	
 	// Update is called once per frame
