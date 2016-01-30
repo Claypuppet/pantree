29d1951bf137d59d91ae6d81676a3c28f2a722d7
Sami Repo <sami.repo@octo3.fi>
2016-01-30 23:49:27 +0200

    Added Textures for different things.

---
+++ UnityProject/PandaTree/Assets/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 5.0f;
+	public float acceleration = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void MovePlayer(float radians, float speedModifier){
+		Vector3 curPos = this.transform.position;
+		Vector3 newPos = newPosition (curPos, radians, speedModifier);
+
+		this.transform.Translate (newPos);
+	}
+
+	Vector3 newPosition(Vector3 curPos, float radians, float speedmodifier){
+		Vector3 newPosition = new Vector3 ();
+
+		newPosition = curPos;
+
+
+
+		return newPosition;
+	}
+}
