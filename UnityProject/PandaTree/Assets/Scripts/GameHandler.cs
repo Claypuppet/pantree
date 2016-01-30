2bd0e8194d8d1caa4a337d49a9b90e6983b13e57
Sami Repo <sami.repo@octo3.fi>
2016-01-30 11:28:22 +0200

    Added Fonts and Canvas. MainMenu added. Toggling of menu. ActionButton added.

--- UnityProject/PandaTree/Assets/Scripts/GameHandler.cs
+++ UnityProject/PandaTree/Assets/Scripts/GameHandler.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class GameHandler : MonoBehaviour {
 
+	public GameObject EnvironmentPrefab;
+
 	// Use this for initialization
 	void Start () {
-	
+		EnvironmentPrefab = GameObject.FindGameObjectWithTag ("Environment");
 	}
 	
 	// Update is called once per frame
