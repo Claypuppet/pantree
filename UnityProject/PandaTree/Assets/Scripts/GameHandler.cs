bea3509ca72314196e8a8576dad048cdf8f45939
Jelle <jelle.bouwhuis@octo3.fi>
2016-01-30 15:05:17 +0200

    Add player collision, renamed player, moved camera in player

--- UnityProject/PandaTree/Assets/Scripts/GameHandler.cs
+++ UnityProject/PandaTree/Assets/Scripts/GameHandler.cs
@@ -5,15 +5,19 @@ using System.Collections.Generic;
 public class GameHandler : MonoBehaviour {
     private GameObject player;
 
-    public GameObject Player { get { return this.player; } set { this.player = value; } }
+    public MainPlayerScript Player { get { return this.player.GetComponent<MainPlayerScript>(); } }
 
 	// Use this for initialization
-	void Start () {
-        
+    void Start() {
+        this.player = GameObject.Find("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    public void updateCanvasInventory() {
+        //this.Player.Inventory
+    }
 }
