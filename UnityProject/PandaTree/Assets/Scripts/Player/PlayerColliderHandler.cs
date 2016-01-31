b33fbc77356e36650c90e6860284062be40242d2
Jelle <jelle.bouwhuis@octo3.fi>
2016-01-31 12:46:40 +0200

    fix collisions

---
+++ UnityProject/PandaTree/Assets/Scripts/Player/PlayerColliderHandler.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerColliderHandler : MonoBehaviour {
+
+    private GameObject collidingObject;
+    private bool isColliding;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void OnCollisionEnter(Collision col) {
+        Debug.Log("collision!");
+        col.gameObject.GetComponent<InteractableCollisionHandler>();
+        this.collidingObject = col.gameObject;
+        this.isColliding = true;
+        col.gameObject.SetActive(false);
+    }
+
+    void onCollisionExit(Collision col) {
+        Debug.Log("no more collision!");
+        this.collidingObject = null;
+        this.isColliding = false;
+    }
+
+    public void Action() {
+        if (this.isColliding && this.collidingObject != null) {
+            this.collidingObject.GetComponent<InteractableCollisionHandler>().Action();
+        }
+    }
+}
--- UnityProject/PandaTree/Assets/Scripts/Player/PlayerController.cs
+++ UnityProject/PandaTree/Assets/Scripts/Player/PlayerController.cs
@@ -5,11 +5,6 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerController : MonoBehaviour {
 
-    public float MoveSpeed = 5f;
-
-    private GameObject collidingObject;
-    private bool isColliding;
-
     Vector3 position;
     Transform myTrans;
 
@@ -38,25 +33,6 @@ public class PlayerController : MonoBehaviour {
             myTrans.Translate(moveHorizontal * MoveSpeed * DeltaTime, 0, 0);
         }
         */
-    }
-
-    void OnCollisionEnter(Collision col) {
-        Debug.Log("collision!");
-        this.collidingObject = col.gameObject;
-        this.collidingObject.GetComponent<InteractableCollisionHandler>().Collide();
-        this.isColliding = true;
-    }
-
-    void onCollisionExit(Collision col) {
-        Debug.Log("no more collision!");
-        this.collidingObject = null;
-        this.isColliding = false;
-    }
-
-    public void Action() {
-        if (this.isColliding && this.collidingObject != null) {
-            this.collidingObject.GetComponent<InteractableCollisionHandler>().Action();
-        }
     }
 
 }
