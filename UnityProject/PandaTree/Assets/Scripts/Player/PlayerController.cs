c9559e0bdc303f6c34b37261926486901c337212
Sami Repo <sami.repo@octo3.fi>
2016-01-31 12:03:24 +0200

    Animations

--- UnityProject/PandaTree/Assets/Scripts/Player/PlayerController.cs
+++ UnityProject/PandaTree/Assets/Scripts/Player/PlayerController.cs
@@ -5,35 +5,18 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerController : MonoBehaviour {
 
-    public float MoveSpeed = 5f;
 
-    Vector3 position;
-    Transform myTrans;
 
     // Use this for initialization
     void Start ()
     {
-        myTrans = this.transform;
-        position = myTrans.transform.position;
-        Debug.Log(position);
+		
 	}
 
     // Update is called once per frame
     void Update()
     {
-        /*
-        float DeltaTime = Time.deltaTime;
-        float moveHorizontal = CrossPlatformInputManager.GetAxis("Horizontal");
-        float moveVertical = CrossPlatformInputManager.GetAxis("Vertical");
-
-        if (moveHorizontal > 0 && moveHorizontal <= 1)
-        {
-            myTrans.Translate(moveHorizontal * MoveSpeed * DeltaTime, 0, 0, 0);
-        }
-        if (moveHorizontal >= -1 && moveHorizontal < 0)
-        {
-            myTrans.Translate(moveHorizontal * MoveSpeed * DeltaTime, 0, 0);
-        }
-        */
+    	    
     }
+		
 }
